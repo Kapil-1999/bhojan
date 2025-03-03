875d166474a888bde0891b8b7279ff64cb9f813c
Skylabs <code@skylabstech.com>
2025-03-03 22:00:29 +0530

    change base url

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -6,8 +6,8 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({});
-    const url = 'http://localhost:4000/api/';
-    const imageUrl = 'http://localhost:4000/'
+    const url = 'https://k-bhojan.onrender.com/api/'; 
+    const imageUrl = 'https://k-bhojan.onrender.com/'
     const [token, setToken] = useState('');
     const [food_list, setFoodList] = useState([])
 
